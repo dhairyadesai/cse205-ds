38a3915828c0b514e927cd4c953961c4fa91523b
Dhairya Desai <dhairya.d3@ahduni.edu.in>
2023-09-25 13:34:56 +0530

    done

---
+++ assignments/04.09.23/234.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return true; // An empty list or a single-node list is a palindrome.
+        }
+
+       
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* secondHalf = reverseList(slow);
+
+       
+        while (head != nullptr && secondHalf != nullptr) {
+            if (head->val != secondHalf->val) {
+                return false;
+            }
+            head = head->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return true;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
